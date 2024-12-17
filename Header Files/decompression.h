e1d70ecca068ae33963b5adcfaf79fae33a7bc7d
Jolieshehata <167891011+Jolieshehata@users.noreply.github.com>
2024-12-17 23:47:44 +0200

    Decompression.h

--- Header Files/decompression.h
+++ Header Files/decompression.h
@@ -1,1 +1,13 @@
+#ifndef DECOMPRESSION_H
+#define DECOMPRESSION_H
 
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+// Function to decode a list of codes using LZW algorithm
+bool decodeFromFile(const std::string& fileName);
+
+#endif // DECOMPRESSION_H
