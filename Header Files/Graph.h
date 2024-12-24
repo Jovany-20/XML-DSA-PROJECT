d5c0cfeb534cba40789ae4fb72705ca693cefea8
Jovany-20 <jovany.josepg@gmail.com>
2024-12-24 16:28:34 +0200

    update graph method
    update graph method

--- Header Files/Graph.h
+++ Header Files/Graph.h
@@ -2,30 +2,14 @@
 #define GRAPH_H
 
 #include "../Header Files/format.h" // Adjust path to include the header
-#include "../Header Files/Tree.h"   // Include Tree class header from the correct folder
+
 #include <vector>
 #include <memory>
 #include <string>
 #include <sstream>
-#include <unordered_map>
-#include <stack>
-#include <algorithm>
+#include <map>
 using namespace std;
 
-
-class Node {
-public:
-    int id;                                         // User ID
-    vector<shared_ptr<Node>> followers;             // List of followers (shared_ptr)
-
-    // Constructor
-    Node(int userId) : id(userId) {}
-
-    // Add follower
-    void addFollower(const shared_ptr<Node>& follower) {
-        followers.push_back(follower);
-    }
-};
-
-vector<shared_ptr<Node>> graphMaker(const string& XML);
+// Function declarations
+void NetworkAnalysis(map<string, vector<string>>& users, vector<string> data);
 #endif // GRAPH_H
--- Source Files/Graph.cpp
+++ Source Files/Graph.cpp
@@ -1,97 +1,30 @@
 #include "../Header Files/Graph.h" // Adjust path to include the header
 
 
-vector<shared_ptr<Node>> graphMaker(const string& XML) {
-    /* @brief This function builds a graph from an XML string representing users and their relationships.
-       @param XML string containing the XML data.
-       @return A vector of shared_ptr<Node> representing the users in the graph.
-    */
+void NetworkAnalysis(map<string, vector<string>>& users, vector<string> data) {
+    string currentUserId = "";       // To store the current user's ID
+    vector<string> followers;        // To store the followers of the current user
+    bool isInsideUser = false;       // Flag to indicate if we're inside a user block
+    bool isInsideFollower = false;   // Flag to indicate if we're inside a follower block
 
-    vector<string> parsingxml = parseXML(XML);
-    Tree* xmlTree = buildTree(parsingxml);
-
-    vector<shared_ptr<Node>> nodes;                     // List of user nodes
-    unordered_map<int, shared_ptr<Node>> idToNodeMap;   // Map to store users by ID for quick lookup
-
-    // Find the <users> node
-    Tree* usersNode = nullptr;
-    for (Tree* child : xmlTree->pointers) {
-        if (child->val == "users") {
-            usersNode = child;
-            break;
+    for (size_t i = 0; i < data.size(); ++i) {
+        if (data[i] == "<user>") {
+            isInsideUser = true;
+            currentUserId = "";      // Reset for the next user
+            followers.clear();       // Clear the followers for the next user
+        } else if (data[i] == "</user>") {
+            isInsideUser = false;
+            if (!currentUserId.empty()) {
+                users[currentUserId] = followers; // Add the user and their followers to the map
+            }
+        } else if (data[i] == "<id>" && isInsideUser && !isInsideFollower) {
+            currentUserId = data[i + 1]; // Extract the user ID
+        } else if (data[i] == "<follower>") {
+            isInsideFollower = true;
+        } else if (data[i] == "</follower>") {
+            isInsideFollower = false;
+        } else if (data[i] == "<id>" && isInsideFollower) {
+            followers.push_back(data[i + 1]); // Extract and store the follower ID
         }
     }
-
-    if (!usersNode) {
-        cout << "No <users> tag found in XML." << endl;
-        delete xmlTree;
-        return {};
-    }
-
-    // Iterate over each <user> node
-    for (Tree* userNode : usersNode->pointers) {
-        if (userNode->val != "user") continue;
-
-        int userId = -1;
-        vector<int> followerIds;
-
-        // Extract user details
-        for (Tree* userDetailNode : userNode->pointers) {
-            if (userDetailNode->val == "id") {
-                // Get user ID
-                if (!userDetailNode->pointers.empty()) {
-                    userId = stoi(userDetailNode->pointers[0]->val);
-                }
-            }
-            else if (userDetailNode->val == "followers") {
-                // Get followers
-                for (Tree* followerNode : userDetailNode->pointers) {
-                    if (followerNode->val == "follower") {
-                        for (Tree* followerDetailNode : followerNode->pointers) {
-                            if (followerDetailNode->val == "id") {
-                                if (!followerDetailNode->pointers.empty()) {
-                                    int followerId = stoi(followerDetailNode->pointers[0]->val);
-                                    followerIds.push_back(followerId);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        if (userId != -1) {
-            // Create a new Node for the user if it doesn't exist
-            shared_ptr<Node> userNodePtr;
-            if (idToNodeMap.find(userId) == idToNodeMap.end()) {
-                userNodePtr = make_shared<Node>(userId);
-                idToNodeMap[userId] = userNodePtr;
-                nodes.push_back(userNodePtr);
-            }
-            else {
-                userNodePtr = idToNodeMap[userId];
-            }
-
-            // Add followers to the user
-            for (int followerId : followerIds) {
-                // Get or create follower node
-                shared_ptr<Node> followerNode;
-                if (idToNodeMap.find(followerId) == idToNodeMap.end()) {
-                    followerNode = make_shared<Node>(followerId);
-                    idToNodeMap[followerId] = followerNode;
-                    nodes.push_back(followerNode);
-                }
-                else {
-                    followerNode = idToNodeMap[followerId];
-                }
-
-                userNodePtr->addFollower(followerNode);
-            }
-        }
-    }
-
-    // Clean up the XML tree
-    delete xmlTree;
-
-    return nodes;
 }
