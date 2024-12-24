1f90b044ff8a0796da54dca7c27280e1f7ccb8a8
Jovany-20 <jovany.josepg@gmail.com>
2024-12-24 16:43:24 +0200

    adding some comments
    adding some comments

--- Header Files/Graph.h
+++ Header Files/Graph.h
@@ -15,9 +15,12 @@ using namespace std;
 // Map to store the user ID , 
 //this will get filled by visulize function (will call NetworkAnalysis)
 //and rest of tasks will only use this map drectly yo save memory.
-
 extern map<string, int> global_map; 
 
 // Function declarations
+// the visulize function will call this function to fill the global map 
+// like this : NetworkAnalysis(global_map, data);
+// it should be called only once by the visulize function. 
+// it must pass the global map to the map parameter.
 void NetworkAnalysis(map<string, vector<string>>& users, vector<string> data);
 #endif // GRAPH_H
