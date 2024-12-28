7427b1fba65dc7262579ff12d55a1593682515ce
Andrew wagdy <167933423+AWx18@users.noreply.github.com>
2024-12-28 14:37:34 +0200

    Create mostActiveUser.cpp

---
+++ Source Files/mostActiveUser.cpp
@@ -0,0 +1,25 @@
+#include "../Header Files/mostActiveUser.h" 
+
+vector<string> mostActiveUser(const map<string, vector<string>>& users) {
+   map<string, int> followerCount;  
+    for (const auto& user : users) {
+        for (const string& follower : user.second) {
+            followerCount[follower]++;  // Increment the follower count
+        }
+    }
+
+    vector<string> mostActiveUsers;
+    int maxFollowers = 0;
+    for (const auto& entry : followerCount) {
+        if (entry.second > maxFollowers) {
+            mostActiveUsers.clear();  
+            mostActiveUsers.push_back(entry.first);
+            maxFollowers = entry.second;
+        }
+        else if (entry.second == maxFollowers) {
+            mostActiveUsers.push_back(entry.first);  
+        }
+    }
+
+    return mostActiveUsers; 
+}
