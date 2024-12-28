f31ec5df7b14bf11b9f3e3fccfe969f9e96a7040
Andrew wagdy <167933423+AWx18@users.noreply.github.com>
2024-12-28 14:34:03 +0200

    Create mostActiveUser.h

---
+++ Header Files/mostActiveUser.h
@@ -0,0 +1,8 @@
+#ifndef MOSTACTIVEUSER_H
+#define MOSTACTIVEUSER_H
+
+#include "../Header Files/Graph.h" // Adjust path to include the header
+
+
+vector<string> mostActiveUser(const map<string, vector<string>>& users);
+#endif // FORMAT_H
